fb79435ab07fc4dd2c7444e57d735df082d352d9
= <=>
2023-06-09 17:10:49 +0530

    made home component

---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+    const portfolioText = {
+        fontSize: "2em",
+        fontWeight: "bold",
+        textAlign: "center",
+        padding: "2em"
+    }
+    return (
+        <div style={portfolioText}>
+            Welcome To My Portfolio
+        </div>
+    )
+}
+
+export default Home;
